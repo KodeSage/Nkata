aacefd56f8dfc6c7a6146362a8fd19f7491c12b0
James Harrison <somtochukwujames2000@gmail.com>
2023-08-22 01:31:30 +0100

    chores: backend server completed

---
+++ client/src/components/ChannellistContainer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Channellist, useChatContext } from "stream-chat-react";
+import { ChannelSearch, TeamChannellist, TeamChannelPreview } from '/';
+
+const Sidebar = () =>
+{
+  <div>
+    
+  </div>
+}
+const ChannellistContainer = () => {
+  return (
+    <div>ChannellistContainer</div>
+  )
+}
+
+export default ChannellistContainer
